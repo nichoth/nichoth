d09c77ec227087a18f3c3bc93de20a0ec9d819ac
nichoth <nichoth@gmail.com>
2020-09-21 21:05:35 -0700

    it actually works

--- build.js
+++ build.js
@@ -1,11 +1,19 @@
-var argv = require('minimist')(process.argv.slice(2))
-var input = argv._[0]
 var hyperstream = require('hyperstream');
 var fs = require('fs');
- 
-var hs = hyperstream({
-    '#a': fs.createReadStream(__dirname + '/a.html'),
-    '#b': fs.createReadStream(__dirname + '/b.html')
-});
-var rs = fs.createReadStream(__dirname + '/index.html');
-rs.pipe(hs).pipe(process.stdout);
+
+var srcPaths = [ 'websites', 'detritus', 'software' ]
+
+srcPaths.forEach(function (path) {
+    var obj = {
+        '#content': fs.createReadStream(__dirname + '/src/' + path + '.html')
+    }
+
+    obj['.site-nav a[href="/' + path + '"]'] = {
+        class: { append: 'active' }
+    }
+
+    var hs = hyperstream(obj)
+    var rs = fs.createReadStream(__dirname + '/src/_index.html')
+    var outPath = __dirname + '/public/' + path + '/index.html'
+    rs.pipe(hs).pipe(fs.createWriteStream(outPath))
+})
--- src/websites.md
+++
@@ -1,7 +0,0 @@
----
-layout: layout.liquid
-pageTitle: This is my Title
----
-
-a paragraph
-
