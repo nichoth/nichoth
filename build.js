fd4488290afd67bd102c4d9083e57e79c417854c
nichoth <nichoth@gmail.com>
2020-10-30 22:19:14 -0700

    build thing

--- build.js
+++ build.js
@@ -4,6 +4,8 @@ var ssbWeb = require('ssb-web')
 var S = require('pull-stream')
 var cat = require('stream-cat')
 var mkdirp = require('mkdirp')
+var slugify = require('@sindresorhus/slugify')
+var after = require('after')
 
 var srcPaths = [ 'websites', 'software' ]
 
@@ -21,6 +23,7 @@ srcPaths.forEach(function (path) {
 
     var hs = hyperstream(obj)
     var rs = fs.createReadStream(__dirname + '/src/_index.html')
+    mkdirp.sync(__dirname + '/public/' + path)
     var outPath = __dirname + '/public/' + path + '/index.html'
     rs.pipe(hs).pipe(fs.createWriteStream(outPath))
 })
@@ -29,25 +32,65 @@ var plugins = [
     require('@nichoth/ssb-tags')({ postType: 'ev.post' })
 ]
 
+function createTagIndex (sbot, tag, msgIds) {
+    mkdirp.sync(__dirname + '/public/detritus/' + tag)
+
+    var posts = ''
+    var next = after(msgIds.length, write)
+    msgIds.forEach(function (id) {
+        sbot.get(id, function (err, msg) {
+            // TODO -- get all mentions
+            var hashSlug = slugify(msg.content.mentions[0].link)
+            posts = posts + `<div class="post">
+                <img src="/posts/img/${hashSlug}">
+                <p>${msg.content.text}</p>
+            </div>`
+
+            // console.log('posts aaaaa', posts)
+            next(null, msg)
+
+            // console.log('*got msg*', err, msg)
+            // console.log('*mentions*', msg.content.mentions)
+        })
+    })
+
+    function write () {
+        var hs = hyperstream({
+            '#content': {
+                _appendHtml: posts
+            }
+        })
+        
+        fs.createReadStream(__dirname + '/src/_index.html')
+            .pipe(hs)
+            .pipe(fs.createWriteStream(__dirname + '/public/detritus/' + tag +
+                '/index.html'))
+    }
+}
+
 // the visual detritus page
 ssbWeb.startSbot('ssb-ev', plugins, function (err, { id, sbot }) {
     if (err) throw err
 
     // pics by tag
     sbot.tags.get(function (err, res) {
-        console.log('tags.get', err, res)
+        console.log('*tags.get*', err, res)
+
+        // todo: make a page for each tag
+        // the page has pics with that tag
+        // need to make nav for the tag pages
+        // `/visual-detritus` has all pics
+        // `/visual-detritus/tag` has pics tagged with `tag`
+        // there is a file `visual-detritus/tag/index.html` for the tag index
+        // the links on the tag index go to that pic's page: `/hash`
+
         Object.keys(res).forEach(function (tag) {
             var msgIds = res[tag]
-            msgIds.forEach(function (id) {
-                sbot.get(id, function (err, msg) {
-                    console.log('got msg', err, msg)
-                    console.log('mentions', msg.content.mentions)
-                })
-            })
+            createTagIndex(sbot, tag, msgIds)
         })
     })
 
-    // this is a concatted list of streams fo html for posts, an index page
+    // this is a concatted list of streams of html for posts, an index page
     var cats = []
     S(
         ssbWeb.getPosts({ id, sbot, type: 'ev.post', reverse: true }),
