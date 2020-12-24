7a78f863424686b99eee2d764a0fd3a012354cf4
Nick Thomas <nichoth@gmail.com>
2020-12-23 20:23:54 -0800

    add examples page

--- build-examples.js
+++ build-examples.js
@@ -6,7 +6,10 @@ var mkdirp = require('mkdirp')
 function examples () {
     mkdirp.sync(__dirname + '/public/examples')
     var hs = hyperstream({
-        '#content': fs.createReadStream(__dirname + '/src/examples.html')
+        '#content': fs.createReadStream(__dirname + '/src/examples.html'),
+        body: {
+            class: { append: 'examples' }
+        }
     })
     var rs = fs.createReadStream(__dirname + '/src/_index.html)
     var outPath = __dirname + '/public/examples/index.html'
