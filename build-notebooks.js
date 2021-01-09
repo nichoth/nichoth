bb455bf052594cdca6dac44f34808afc309e035a
nichoth <nichoth@gmail.com>
2021-01-08 17:42:18 -0800

    small notebook images

--- build-notebooks.js
+++ build-notebooks.js
@@ -3,6 +3,7 @@ var minimist = require('minimist');
 const mkdirp = require('mkdirp');
 var path = require('path')
 const hyperstream = require('hyperstream');
+var sharp = require('sharp')
 
 function notebooks (cb) {
     // process.stdin.pipe(process.stdout)
@@ -21,6 +22,23 @@ function notebooks (cb) {
             path.basename(fileName))
         fs.createReadStream(fileName)
             .pipe(imgWS)
+
+
+        // ---------- create a small version ------------
+        const transformer = sharp().withMetadata().resize({
+            width: 300,
+            height: 300,
+            fit: sharp.fit.outside,
+            position: sharp.strategy.entropy,
+            withoutEnlargement: true
+        })
+
+        fs.createReadStream(fileName)
+            .pipe(transformer)
+            .pipe(fs.createWriteStream(__dirname + '/public/img/notebooks/' +
+                path.basename(fileName, '.jpg') + '-sm.jpg')
+            )
+        // -------------------------------------------------------------
 
 
         // create a page for that image only
@@ -50,9 +68,10 @@ function notebooks (cb) {
 
 
         // create html for the index page
+        var smallPic = path.basename(fileName, '.jpg') + '-sm.jpg'
         html += `<li>
             <a href="/notebooks/${path.basename(fileName, '.jpg')}">
-                <img src="/img/notebooks/${path.basename(fileName)}">
+                <img src="/img/notebooks/${smallPic}">
             </a>
         </li>
         `
@@ -76,6 +95,7 @@ function notebooks (cb) {
     fs.createReadStream(__dirname + '/src/_index.html')
         .pipe(hs)
         .pipe(indexWS)
+        .once('close', cb)
 }
 
 module.exports = notebooks
