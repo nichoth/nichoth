432f8c71974f1d93249ca4365635a7c4e5a10281
nichoth <nichoth@gmail.com>
2022-04-09 15:16:26 -0700

    add copy button

--- src/ssc/index.js
+++ src/ssc/index.js
@@ -1,10 +1,28 @@
 import { create, exportPrivateKey } from 'ssc-keys'
 
-const linkEl = document.getElementById('ntl-button')
+// const linkEl = document.getElementById('ntl-button')
 
 create().then(kp => {
     const exported = exportPrivateKey(kp)
-    const linkWithPrivKey = (linkEl.href + '#PRIV_KEY=' + exported)
+
+    const newEl = document.createElement('code')
+    const newContent = document.createTextNode(exported);
+    newEl.appendChild(newContent)
+
+    const container = document.getElementById('the-private-key')
+    container.appendChild(newEl)
+
+    document.getElementById('copy-priv-key').addEventListener('click', ev => {
+        ev.preventDefault()
+        navigator.clipboard.writeText(exported)
+        document.getElementById('copy-priv-key').insertAdjacentHTML(
+            'afterend',
+            '<span class="copied">copied!</span>'
+        )
+    })
+    
+    // const linkWithPrivKey = (linkEl.href + '#PRIV_KEY=' + exported)
         // encodeURIComponent(exported))
-    document.getElementById('ntl-button').href = linkWithPrivKey
+    // document.getElementById('ntl-button').href = linkWithPrivKey
 })
+
