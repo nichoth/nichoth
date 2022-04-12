1cde40a5112bf67722e57e1e326c0a6431255385
nichoth <nichoth@gmail.com>
2022-04-12 07:25:50 -0700

    rename things

--- src/postcards/_index.js
+++ src/postcards/_index.js
@@ -8,9 +8,24 @@ const ws = fs.createWriteStream(path.resolve(__dirname, '..', '..', 'public',
     'postcards', 'index.html'))
 
 const hs = hyperstream({
+    'title': 'nichoth | postcards',
+
+    'head': {
+        _appendHtml: `<meta property="og:title" data-rh="true"
+            content="nichoth | postcards">
+        
+        <meta property="og:description" data-rh="true"
+            content="Postcards" name="description">
+        
+        <meta property="og:image" data-rh="true"
+            content="https://nichoth.com/img/cube.png">
+        `
+    },
+
     'body': {
         class: { append: 'postcards' }
     },
+
     '#content': {
         _appendHtml: `<p>foo bar</p>`
     }
