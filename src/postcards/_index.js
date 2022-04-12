cf43861efcabdcecb547b86a0a81df5c090fe1d9
nichoth <nichoth@gmail.com>
2022-04-12 08:14:37 -0700

    better scss variables

--- src/postcards/_index.js
+++ src/postcards/_index.js
@@ -1,34 +1,76 @@
 var hyperstream = require('hyperstream')
 var fs = require('fs')
-// var glob = require("glob")
+var glob = require("glob")
 const path = require('path')
 
 const rs = fs.createReadStream(path.resolve(__dirname, '..', '_index.html'))
 const ws = fs.createWriteStream(path.resolve(__dirname, '..', '..', 'public',
     'postcards', 'index.html'))
 
-const hs = hyperstream({
-    'title': 'nichoth | postcards',
+getPostcardHtml((err, html) => {
+    // console.log('wooo', err, html)
 
-    'head': {
-        _appendHtml: `<meta property="og:title" data-rh="true"
-            content="nichoth | postcards">
-        
-        <meta property="og:description" data-rh="true"
-            content="Postcards" name="description">
-        
-        <meta property="og:image" data-rh="true"
-            content="https://nichoth.com/img/cube.png">
-        `
-    },
+    // files.forEach(file => {
+    //     // var name = path.basename(file, '.png')
+    //     var name = path.basename(file)
+    //     console.log('*name*', name)
+    // })
 
-    'body': {
-        class: { append: 'postcards' }
-    },
-
-    '#content': {
-        _appendHtml: `<p>foo bar</p>`
-    }
+    const hs = makeHs(html)
+    rs.pipe(hs).pipe(ws)
 })
 
-rs.pipe(hs).pipe(ws)
+function makeHs (content) {
+    const hs = hyperstream({
+        'title': 'nichoth | postcards',
+
+        'head': {
+            _appendHtml: `<meta property="og:title" data-rh="true"
+                content="nichoth | postcards">
+            
+            <meta property="og:description" data-rh="true"
+                content="Postcards" name="description">
+            
+            <meta property="og:image" data-rh="true"
+                content="https://nichoth.com/img/cube.png">
+            `
+        },
+
+        'body': {
+            class: { append: 'postcards' }
+        },
+
+        '#content': {
+            _appendHtml: content
+        }
+    })
+
+    return hs
+}
+
+
+function getPostcardHtml (cb) {
+    const picPath = path.join(__dirname, '..', '..', 'public', 'postcards',
+        '*.png')
+
+    glob(picPath, (err, files) => {
+        if (err) return cb(err)
+
+        // console.log('*files*', files)
+
+        var filesHtml = files.reduce((acc, file) => {
+            return acc + `<li class="postcard">
+                <img src="/postcards/${path.basename(file)}">
+                <p>${path.basename(file, '.png')}</p>
+            </li>`
+        }, '<ul>')
+
+        filesHtml += '</ul>'
+
+        // cb(null, files.reduce((acc, file) => {
+        //     return acc + ``
+        // }), '')
+
+        cb(null, filesHtml)
+    })
+}
