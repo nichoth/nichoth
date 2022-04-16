0be3da9423ba33e895210fb8f93b5e9f5ad27d93
nichoth <nichoth@gmail.com>
2022-04-16 13:37:46 -0700

    update postcard copy

--- src/postcards/_index.js
+++ src/postcards/_index.js
@@ -71,16 +71,36 @@ const ws = fs.createWriteStream(path.resolve(__dirname, '..', '..', 'public',
 const html = getPostcardHtml()
 const withWords = `
     <h1>Postcards</h1>
+
     <p>
         You can purchase <a href="https://opensea.io/collection/postcards">
         these on the internet.</a>
     </p>
+
     <p>
         I made these postcards in the past when I was a student. I worked in a
         photo studio/laboratory at the time, and this was a fun way to pass
-        the time, printing little postcards on the photo printers. I noticed
-        that people enjoy them.
+        the time, printing silly postcards on the fancy photo printers.
+        I noticed that people enjoy them.
     </p>
+
+    <p>
+        On one hand this was kind of interesting just to see what it's like
+        for a regular civilian (non computer person) to create an NFT. The thing
+        that surprised me is <em>how much trust</em> is involved in the
+        trustless ecosystem. This is coming from someone who read and
+        appreciated
+        <a href="https://moxie.org/2022/01/07/web3-first-impressions.html">
+            Moxie's article
+        </a>
+    </p>
+
+    <p>
+        People are trusting me to not change a URL. And the whole thing feels
+        very <em>early internet</em>, if that's a thing. I made some HTML for each NFT
+        more or less by hand, and it is hosted on my "personal web page".
+    </p>
+
     <p>
         If you want, I can send a real, physical copy of the postcard to you
         in the mail.
@@ -95,7 +115,7 @@ const withWords = `
         that even most insurance, if you do have it, simply does not cover.
         They have what is called a "TMJ exclusion", which means they wont help
         you with it at all. So the long story is that I just need money to pay
-        for our lives, and this is my being desperate with how to get ahold of
+        for our lives, and this is me being desperate with how to get ahold of
         it.
 
         Sorry it's dark, but should be ok since this is the monologue.
@@ -108,7 +128,8 @@ const withWords = `
         Most NFTs I've seen seem to be computer-generated avatar like things.
         Which is kind of interesting because the artwork is more the fact that
         someone with money thinks it's cool to spend it frivolously than the
-        picture itself… I could think about that too much; former art major, etc 
+        picture itself… I could think about that too much; I'm a former art
+        major, etc 
     </p>
 ` + html
 
